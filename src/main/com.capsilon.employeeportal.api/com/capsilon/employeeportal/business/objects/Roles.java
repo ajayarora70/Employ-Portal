f0d0b258d475bcb36590a7dd0ba1083c7e3f0571
tpatel <tanmay.patel@capsilon.com>
2019-06-17 15:38:50 +0530

    removed unnecessary files

--- src/main/com.capsilon.employeeportal.api/com/capsilon/employeeportal/business/objects/Roles.java
+++
@@ -1,8 +0,0 @@
-package com.capsilon.employeeportal.business.objects;
-
-public enum Roles {
-
-	HR, ADMIN, EMPLOYEE;
-	
-	//List<String> allowedESignatureProviders = Stream.of(EsignatureProvider.values()).map(Enum::name).collect(Collectors.toList());
-}

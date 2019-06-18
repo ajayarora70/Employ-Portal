4b5168764f57b5a08cf61449c4522508145c68b5
tpatel <tanmay.patel@capsilon.com>
2019-06-18 11:47:59 +0530

    Added CORS filter

--- src/main/java/com/capsilon/employeeportal/config/Config.java
+++ src/main/java/com/capsilon/employeeportal/config/Config.java
@@ -1,11 +1,17 @@
 package com.capsilon.employeeportal.config;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import com.mongodb.MongoClient;
 
@@ -37,5 +43,18 @@ class Config extends AbstractMongoConfiguration
 	protected String getDatabaseName()
 	{
 		return database;
+	}
+
+	@Bean
+	public CorsFilter corsFilter()
+	{
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOrigins(Collections.singletonList("*"));
+		config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
 	}
 }
